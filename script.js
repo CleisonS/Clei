86a41dbccd8026957f15021b45452d8c58ede8bb
CleisonS <83415172+CleisonS@users.noreply.github.com>
2023-07-28 01:52:16 -0300

    pause

--- script.js
+++ script.js
@@ -4,18 +4,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const nextButton = document.getElementById("nextButton");
   
     const musicList = [
-        "music/dust.mp3",
-        "music/memory.mp3",
-        "music/Shadow.mp3",
-        "music/blizzard.mp3",
-        "music/crystal.mp3"
+      "music/dust.mp3",
+      "music/memory.mp3",
+      "music/Shadow.mp3",
+      "music/blizzard.mp3",
+      "music/crystal.mp3"
     ];
     let currentMusicIndex = 0;
+    let savedPlaybackTime = 0; // Variable to store the paused playback time
   
     function playCurrentMusic() {
       audioElement.src = musicList[currentMusicIndex];
+      audioElement.currentTime = savedPlaybackTime; // Set the playback time to the saved value
       audioElement.play();
-      playButton.textContent = "Pause";
+      playButton.innerHTML = '<i class="fas fa-pause"></i>';
     }
   
     function setMusicIndexToLocalStorage() {
@@ -23,41 +25,44 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   
     function setPlayButtonIcon() {
-        if (audioElement.paused) {
+      if (audioElement.paused) {
         playButton.classList.remove("pause-icon");
         playButton.classList.add("play-icon");
-        playButton.innerHTML = '<i class="fas fa-play"></i>'; // Ícone de Play
-        } else {
+        playButton.innerHTML = '<i class="fas fa-play"></i>';
+      } else {
         playButton.classList.remove("play-icon");
         playButton.classList.add("pause-icon");
-        playButton.innerHTML = '<i class="fas fa-pause"></i>'; // Ícone de Pause
-        }
+        playButton.innerHTML = '<i class="fas fa-pause"></i>'; 
+      }
     }
-
-  setPlayButtonIcon();
-
-  playButton.addEventListener("click", function () {
-    if (audioElement.paused) {
-      playCurrentMusic();
-    } else {
+    setPlayButtonIcon();
+  
+    playButton.addEventListener("click", function () {
+      if (audioElement.paused) {
+        playCurrentMusic();
+      } else {
+        savedPlaybackTime = audioElement.currentTime; 
+        audioElement.pause();
+        setPlayButtonIcon();
+      }
+    });
+  
+    nextButton.addEventListener("click", function () {
       audioElement.pause();
-      setPlayButtonIcon(); // Atualiza o ícone do botão ao pausar
-    }
-  });
-  nextButton.addEventListener("click", function () {
-    audioElement.pause();
-    currentMusicIndex++;
-    if (currentMusicIndex >= musicList.length) {
-      currentMusicIndex = 0;
-    }
-    playCurrentMusic();
-  });
-  audioElement.addEventListener("ended", function () {
-    currentMusicIndex++;
-    if (currentMusicIndex >= musicList.length) {
-      currentMusicIndex = 0;
-    }
-    playCurrentMusic();
-  });
+      currentMusicIndex++;
+      if (currentMusicIndex >= musicList.length) {
+        currentMusicIndex = 0;
+      }
+      playCurrentMusic();
+    });
+  
+    audioElement.addEventListener("ended", function () {
+      currentMusicIndex++;
+      if (currentMusicIndex >= musicList.length) {
+        currentMusicIndex = 0;
+      }
+      playCurrentMusic();
+    });
+  
     window.addEventListener("beforeunload", setMusicIndexToLocalStorage);
   });
